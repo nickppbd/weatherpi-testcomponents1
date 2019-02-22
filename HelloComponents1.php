bc8e60272bee4bd0ee1f80ccb8dc544e59139ad2
nickppbd <nickppbd@gmail.com>
2019-02-22 19:55:46 +0900

    Add files via upload

---
+++ HelloComponents1.php
@@ -0,0 +1,12 @@
+<?php
+namespace Weatherpi\component1;
+
+class HelloComponents1
+{
+    protected $name = "components1";
+
+    public function getName()
+    {
+        return $this->name;
+    }
+}
